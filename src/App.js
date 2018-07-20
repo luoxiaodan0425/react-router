113787f16ca57f54b727b069bc5a7ae2b5d01da7
wangh <wangh147>
2018-07-20 16:14:22 +0800

    ad

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
 import Home from './pages/Home';
-// import About from './pages/About.js';
-// import Contact from './pages/Contact.js';
+import{
+  BrowserRouter as Router,
+  // HashRouter as Router,
+  Route
+}from 'react-router-dom'
+import About from './pages/About.js';
+import Contact from './pages/Contact.js';
 
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import Jumbotron from './components/Jumbotron';
 class App extends Component {
   render() {
     return (
-      <div>
-        <Home />
-      </div>
+      <Router>
+        <div>
+          <Navbar />
+          <Jumbotron title="About Me!" subtitle="This page is all about me and my work!"/>
+          <Route exact path="/" component={Home} />
+          <Route path="/contact/:id" component={Contact} />
+          <Route path="/about" component={About} />
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
